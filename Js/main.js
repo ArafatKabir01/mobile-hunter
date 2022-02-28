91aa6a2193847cc9aecd29db825e9a7158afc1a4
ak nabil <nabil786.ak@gmail.com>
2022-03-01 03:01:28 +0600

    see more btn add

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,22 @@
 const searchPhone = () => {
+    const phoneCard = document.getElementById('phone-card-center');
+    phoneCard.innerHTML=``;
     const userInput = document.getElementById('src-input-field').value;
     const inputLowerCase = userInput.toLowerCase();
     const PhoneApiUrl = `https://openapi.programming-hero.com/api/phones?search=${inputLowerCase}`
     fetch(PhoneApiUrl)
     .then(res => res.json())
-    .then(data => phoneItems(data.data))
+    .then(data => phoneItems(data.data.slice(0,20)))
+
+};
+// See more button funcation
+const moreItem = () =>{
+    const PhoneApiUrl = `https://openapi.programming-hero.com/api/phones?search=`
+    fetch(PhoneApiUrl)
+    .then(res => res.json())
+    .then(data => phoneItems(data.data.slice(20, )))
+    const seeMoreDiv = document.getElementById('SeeMore');
+    seeMoreDiv.classList.add('d-none')
 };
 
 const phoneItems = phones => {
@@ -26,7 +38,9 @@ const phoneItems = phones => {
         </div>
         `;
         phoneCard.appendChild(newDiv);
+
     });
 
 }
+
 searchPhone();

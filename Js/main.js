de6178def03097df6bd0065044719201a746b102
ak nabil <nabil786.ak@gmail.com>
2022-03-02 15:50:18 +0600

    final update

--- Js/main.js
+++ Js/main.js
@@ -105,36 +105,54 @@ const detailsBtn = phoneId => {
 
 };
 const detailsfunction = phonesId => {
+  console.log(phonesId.others)
     const detailsDiv = document.getElementById('details-section')
-    detailsDiv.innerHTML=`
-    <div class="card mb-3" style="width: 740px;">
-    <div class="row g-0">
-      <div class="col-4 m-3 ps-3 text-center ">
-        <img src="${phonesId.image}" class="w-100 h-80 " alt="...">
-      </div>
-      <div class="col-md-6 ms-xl-6">
-        <div class="card-body ">
-          <h5 class="card-title text-black ">${phonesId.name}</h5><br>
-          <h5 class="card-title text-black "><u>Specifications:</u></h5>
-          <p class='text-black'><span class="fw-bold">Chip Set : </span>${phonesId.mainFeatures.chipSet}</p>
-          <p class='text-black'><span class="fw-bold">Display Size : </span>${phonesId.mainFeatures.displaySize}</p>
-          <p class='text-black'><span class="fw-bold">Memory : </span>${phonesId.mainFeatures.memory}</p>
-          <p class='text-black'><span class="fw-bold">Storage : </span>${phonesId.mainFeatures.storage}</p>
-          <p class='text-black'><span class="fw-bold">sensors : </span>${phonesId.mainFeatures.sensors[0]},
-          <span>${phonesId.mainFeatures.sensors[1]},</span><span>${phonesId.mainFeatures.sensors[2]},</span><span>${phonesId.mainFeatures.sensors[3]},</span><span>${phonesId.mainFeatures.sensors[4]}</span></p>
-          <p class='text-black'><span class="fw-bold">Bluetooth : </span>${phonesId.others.Bluetooth}?</p>
-          <p class='text-black'><span class="fw-bold">GPS : </span>${phonesId.others.GPS}</p>
-          <p class='text-black'><span class="fw-bold">NFC : </span>${phonesId.others.NFC}</p>
-          <p class='text-black'><span class="fw-bold">Radio : </span>${phonesId.others.Radio}</p>
-          <p class='text-black'><span class="fw-bold">USB : </span>${phonesId.others.USB}</p>
-          <p class='text-black'><span class="fw-bold">WLAN : </span>${phonesId.others.WLAN}</p>
-          <p class='text-black'><span class="fw-bold">Release Date : </span>${phonesId.releaseDate ? phonesId.releaseDate : releaseDate="Comming Soon"}
-        </div>
+    if(phonesId.others == undefined){
+      loadingSpinner('none');
+      const errorMsg = document.getElementById('error-msg');
+      errorMsg.innerHTML=`
+      <div class="toast align-items-center border border-danger d-block" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body">
+        <p class = 'text-danger pt-2'>No Data Found...Try next Time</p>
+       </div>
+        <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
       </div>
     </div>
-  </div>  
-    `;
-    loadingSpinner('none');
+      `;
+    }
+    else{
+      detailsDiv.innerHTML=`
+      <div class="card mb-3" style="width: 740px;">
+      <div class="row g-0">
+        <div class="col-4 m-3 ps-3 text-center ">
+          <img src="${phonesId.image}" class="w-100 h-80 " alt="...">
+        </div>
+        <div class="col-md-6 ms-xl-6">
+          <div class="card-body ">
+            <h5 class="card-title text-black ">${phonesId.name}</h5><br>
+            <h5 class="card-title text-black "><u>Specifications:</u></h5>
+            <p class='text-black'><span class="fw-bold">Chip Set : </span>${phonesId.mainFeatures.chipSet}</p>
+            <p class='text-black'><span class="fw-bold">Display Size : </span>${phonesId.mainFeatures.displaySize}</p>
+            <p class='text-black'><span class="fw-bold">Memory : </span>${phonesId.mainFeatures.memory}</p>
+            <p class='text-black'><span class="fw-bold">Storage : </span>${phonesId.mainFeatures.storage}</p>
+            <p class='text-black'><span class="fw-bold">sensors : </span>${phonesId.mainFeatures.sensors[0]},
+            <span>${phonesId.mainFeatures.sensors[1]},</span><span>${phonesId.mainFeatures.sensors[2]},</span><span>${phonesId.mainFeatures.sensors[3]},</span><span>${phonesId.mainFeatures.sensors[4]}</span></p>
+            <p class='text-black'><span class="fw-bold">Bluetooth : </span>${phonesId.others.Bluetooth}?</p>
+            <p class='text-black'><span class="fw-bold">GPS : </span>${phonesId.others.GPS}</p>
+            <p class='text-black'><span class="fw-bold">NFC : </span>${phonesId.others.NFC}</p>
+            <p class='text-black'><span class="fw-bold">Radio : </span>${phonesId.others.Radio}</p>
+            <p class='text-black'><span class="fw-bold">USB : </span>${phonesId.others.USB}</p>
+            <p class='text-black'><span class="fw-bold">WLAN : </span>${phonesId.others.WLAN}</p>
+            <p class='text-black'><span class="fw-bold">Release Date : </span>${phonesId.releaseDate ? phonesId.releaseDate : releaseDate="Comming Soon"}
+          </div>
+        </div>
+      </div>
+    </div>  
+      `;
+      loadingSpinner('none');
+    }
+
 };
 
 const loadingSpinner = display =>{
